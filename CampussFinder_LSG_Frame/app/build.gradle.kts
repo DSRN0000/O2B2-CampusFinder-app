7cf60fc56670e18c681bcfefb2782171f9987e34
seungdeng <113653735+seungdeng@users.noreply.github.com>
2024-11-06 23:47:52 +0900

    수정ver

--- CampussFinder_LSG_Frame/app/build.gradle.kts
+++ CampussFinder_LSG_Frame/app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+        encoding = "UTF-8"
     }
 }
 
@@ -40,4 +41,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
